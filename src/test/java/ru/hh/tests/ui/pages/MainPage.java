b25af70780ad7f371aab8af02622a18ca4d73c33
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-05-05 18:34:42 +0300

    deleted language tests and package

--- src/test/java/ru/hh/tests/ui/pages/MainPage.java
+++ src/test/java/ru/hh/tests/ui/pages/MainPage.java
@@ -1,31 +1,20 @@
 package ru.hh.tests.ui.pages;
 
 import com.codeborne.selenide.SelenideElement;
-import ru.hh.data.Language;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class MainPage {
 
     SelenideElement searchInput = $("[data-qa = 'search-input']"),
-                    submitSearch = $("[data-qa = 'search-button']"),
-                    titleJobForEvery = $("[data-qa = 'bloko-header-3']");
+                    submitSearch = $("[data-qa = 'search-button']");
 
     public void openPage() {
-        open("/?hhtmFrom=");
-    }
-
-    public void switchLanguageWithCheck(Language language) {
-        openPage();
-        $("[data-qa = 'change-locale-" + language + "']").click();
-
-        titleJobForEvery.shouldHave(text(language.description));
+        open("");
     }
 
     public void searchInput(String value) {
-        openPage();
         searchInput.setValue(value);
         submitSearch.click();
     }
