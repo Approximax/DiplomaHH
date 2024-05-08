7783cd5377d376e83fb1ac839e8621a281ae3565
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-05-09 00:07:25 +0300

    added tasks for gradle and missing annotations for employers test

--- src/test/java/ru/hh/tests/api/EmployersTests.java
+++ src/test/java/ru/hh/tests/api/EmployersTests.java
@@ -1,5 +1,8 @@
 package ru.hh.tests.api;
 
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.hh.helpers.RandomData;
 import ru.hh.tests.TestBase;
@@ -12,10 +15,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static ru.hh.tests.api.specs.HhSpecs.requestSpec;
 import static ru.hh.tests.api.specs.HhSpecs.responseSpec;
 
+@Tag("employers_API")
+@Owner("Vladislav Kryukov")
 public class EmployersTests extends TestBase {
     RandomData randomData = new RandomData();
 
     @Test
+    @DisplayName("Search results for employers more than 0")
     void employersFoundCountTest() {
         EmployerRequestModel body = new EmployerRequestModel();
         body.setText(randomData.getRandomEmployer());
